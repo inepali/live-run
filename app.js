79cd88d6a8ffc94b1741a56915afdf228971edbf
iNepali <sanjayaghimire@gmail.com>
2017-05-09 06:39:10 -0500

    Updates

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //variables
 var connections = [];
-var roomNum = 1;
-var roomSize = 8
+var rooms = [];
 
 var express = require("express");
 var app = express();
@@ -25,9 +24,16 @@ io.sockets.on('connection', function (socket) {
     connections.push(socket.id);
 
     log('Connection started ' + socket.id + ', # of connections ' + connections.length);
-
+    //io.sockets.emit('UPDATE_ONLY_ROOMS', rooms);
+    io.sockets.connected[socket.id].emit('UPDATE_ONLY_ROOMS', rooms);
 
     socket.on('disconnect', function (data) {
+        //log(connections);
+        for (var i = 0; i < connections.length; i++) {
+            if (connections[i] == socket.id) {
+                connections.splice(i, 1);
+            }
+        }
 
         log('Connection lost ' + socket.id + ', # of connections ' + connections.length);
 
@@ -43,15 +49,34 @@ io.sockets.on('connection', function (socket) {
         io.sockets.emit('UPDATE_RUN', data);
     });
 
-     //let create room 
+     //when runner create a room 
     socket.on('START_RUN', function (data) {
         io.sockets.emit('START_RUN', data);
         //io.sockets.to(player.room).emit('UPDATE_GAME', { players: players, thisPlayer: player, choiceCard: null });
     });
     
-   //let create room 
+   //let when someone join room 
     socket.on('JOIN_ROOM', function (data) {
         io.sockets.emit('JOIN_ROOM', data);
+        //io.sockets.to(player.room).emit('UPDATE_GAME', { players: players, thisPlayer: player, choiceCard: null });
+    });
+
+
+    //let when someone join room 
+    socket.on('UPDATE_ROOMS', function () {
+        io.sockets.connected[socket.id].emit('UPDATE_ROOMS', rooms);
+        //io.sockets.to(player.room).emit('UPDATE_GAME', { players: players, thisPlayer: player, choiceCard: null });
+    });
+
+    //let when runner create room
+    socket.on('CREATE_ROOM', function (room) {
+        console.log(room);
+        rooms.push(room);
+
+        console.log(rooms.length);
+        
+        io.sockets.emit('UPDATE_ROOMS', rooms);
+        
         //io.sockets.to(player.room).emit('UPDATE_GAME', { players: players, thisPlayer: player, choiceCard: null });
     });
 });
